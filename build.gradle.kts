379a27994e827728d34b36bc30811f81fdf0cbfa
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-01-23 13:43:02 -0500

    Add Publishing Plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
+import com.hypherionmc.modpublisher.properties.CurseEnvironment
+import com.hypherionmc.modpublisher.properties.ModLoader
+import com.hypherionmc.modpublisher.properties.ReleaseType
+
 plugins {
 	id("fabric-loom") version "1.5-SNAPSHOT"
+	id("com.hypherionmc.modutils.modpublisher") version "2.+"
 	`maven-publish`
 	java
 	idea
@@ -39,4 +44,29 @@ tasks {
 	}
 
 	jar.get().exclude("io/github/jt122406/datagen/**")
+}
+
+publisher {
+	apiKeys {
+		curseforge(getPublishingCredentials().first)
+		modrinth(getPublishingCredentials().second)
+		github(project.properties["github_token"].toString())
+	}
+	curseID.set("965876")
+	modrinthID.set("null")
+	githubRepo.set("https://github.com/JT122406/ForgeTagsOnFabric")
+	setReleaseType(ReleaseType.BETA)
+	version.set(project.version.toString())
+	displayName.set("${base.archivesName.get()}-${version.get()}")
+	changelog.set("test changelog")
+	artifact.set(tasks.remapJar)
+	setGameVersions(minecraftVersion)
+	setLoaders(ModLoader.FABRIC, ModLoader.QUILT)
+	setCurseEnvironment(CurseEnvironment.SERVER)
+}
+
+private fun getPublishingCredentials(): Pair<String?, String?> {
+	val curseForgeToken = (project.findProperty("curseforge_token") ?: System.getenv("CURSEFORGE_TOKEN") ?: "") as String?
+	val modrinthToken = (project.findProperty("modrinth_token") ?: System.getenv("MODRINTH_TOKEN") ?: "") as String?
+	return Pair(curseForgeToken, modrinthToken)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ pluginManagement.repositories {
 		name = "Fabric"
 		url = uri("https://maven.fabricmc.net/")
 	}
+	maven("https://maven.firstdarkdev.xyz/releases")
 	mavenCentral()
 	gradlePluginPortal()
 }
