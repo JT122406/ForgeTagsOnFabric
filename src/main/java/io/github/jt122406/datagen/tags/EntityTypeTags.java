5e63620af1b9a8147b9db6a32e84098fc6bad6a6
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-01-23 13:06:22 -0500

    Add Entitytypes to forge tag

--- src/main/java/io/github/jt122406/datagen/tags/EntityTypeTags.java
+++ src/main/java/io/github/jt122406/datagen/tags/EntityTypeTags.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.fabricmc.fabric.api.tag.convention.v1.ConventionalEntityTypeTags;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.world.entity.EntityType;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -17,5 +18,7 @@ public class EntityTypeTags extends FabricTagProvider.EntityTypeTagProvider {
     protected void addTags(HolderLookup.Provider arg) {
         getOrCreateTagBuilder(ConventionalEntityTypeTags.BOSSES)
                 .addOptionalTag(Tags.EntityTypes.BOSSES);
+
+        getOrCreateTagBuilder(Tags.EntityTypes.BOSSES).add(EntityType.ENDER_DRAGON, EntityType.WITHER);
     }
 }
